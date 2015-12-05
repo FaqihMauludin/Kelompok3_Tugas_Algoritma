ada61e44e4b09a1fc900f122db858ddfc1bf861d
FaqihMauludin <faqihmauludin@gmail.com>
2015-12-05 09:03:58 +0700

    Create Script_Dasar.pas

---
+++ Script_Dasar.pas
@@ -0,0 +1,22 @@
+program index_nilai;
+{I.S. : }
+{F.S. : }
+uses Crt;
+
+const
+    MaksM = 50; // maksimal baris
+    MaksN = 8; // maksimal kolom nilai mata kuliah
+
+type
+    Larik1 = Array [1..MaksN] of String;
+    Larik2 = Array [1..MaksM] of String;
+
+var
+    M, N : Integer;
+
+// TARUH CLUE DARI PRODECURE FUNCTION MASING-MASING DISINI
+
+// algoritma utama
+begin
+    panggil_nama_procedure_atau_fungsi_disini();
+end.
